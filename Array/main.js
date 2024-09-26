856d3a9d97dcb2639417ccb6f36257b8decff21f
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-09-26 07:07:41 +0600

    array - reverse an array of integers

---
+++ Array/main.js
@@ -0,0 +1,15 @@
+/**
+ * 01. Reverse an array of integers.
+ */
+const arr1 = [1, 2, 3, 4];
+// process 01
+// arr1.reverse()
+
+// process 02
+// let newArr = [];
+// for (let i = 0; i < arr1.length; i++) {
+//   newArr.unshift(arr1[i]);
+// }
+// console.log("New Array ", newArr);
+
+// process 03
